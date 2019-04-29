b0941fd2f8927814da2d30799b756a18ba063ea8
yuta sakamoto <sorigeeeen@gmail.com>
2019-04-29 23:44:54 +0900

    add marker for mom page

--- arapp/views.py
+++ arapp/views.py
@@ -4,9 +4,11 @@ from django.views import View
 # Create your views here.
 class TopView(View):
   def get(self, request, *args, **kwargs):
-    context = {
-      'message': "Hello World!",
-    }
-    return render(request, 'arapp/top.html', context)
+    return render(request, 'arapp/top.html')
+
+class MotherDayView(View):
+  def get(self, request, *args, **kwargs):
+    return render(request, 'arapp/mom.html')
 
 top = TopView.as_view()
+mother = MotherDayView.as_view()
